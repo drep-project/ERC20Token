efc84de2330108225ac5ea732302cf17f2133bc0
Eric Chao <ericchao@drep.org>
2018-05-15 21:42:18 +0800

    Add require to avoid attack

--- drep_token.sol
+++ drep_token.sol
@@ -60,6 +60,7 @@ contract DrepToken {
     }
 
     function approve(address _spender, uint256 _value) isRunning validAddress public returns (bool success) {
+        require(_value == 0 || allowance[msg.sender][_spender] == 0);
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
